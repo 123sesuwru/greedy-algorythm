ae81a5f4b0391717c75ded1d407c0127d734ce77
123sesuwru <94315065+123sesuwru@users.noreply.github.com>
2022-12-25 20:19:47 +0200

    Add files via upload

---
+++ greedy_algo.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int sum=0;
+    cout<<"Welcome to MMM! Please deposit ur cash, DW we will return  xd :  ";
+    cin>>sum;
+
+    int banknotes[7] = {50, 100, 200, 500, 1000};
+    
+
+
+}
